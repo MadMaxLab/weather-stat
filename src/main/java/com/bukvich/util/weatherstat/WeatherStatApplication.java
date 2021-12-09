8fb28798f53f97cf23b5428aeab0fbdf170136ee
Maxim Bukvich <m.bukvich@conundrum.ai>
2021-12-09 17:10:09 +0300

    Init commit

---
+++ src/main/java/com/bukvich/util/weatherstat/WeatherStatApplication.java
@@ -0,0 +1,13 @@
+package com.bukvich.util.weatherstat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SuppressWarnings("PMD")
+@SpringBootApplication
+public class WeatherStatApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WeatherStatApplication.class, args);
+  }
+}
---
+++ src/test/java/com/bukvich/util/weatherstat/WeatherStatApplicationTests.java
@@ -0,0 +1,11 @@
+package com.bukvich.util.weatherstat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherStatApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
